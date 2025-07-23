e79032ef51a3f8612236001da3800d233832003e
mohit <mohitkumawat221004@gmail.com>
2025-07-23 22:50:28 +0530

    update

--- creatfileapp.js
+++ creatfileapp.js
@@ -1,3 +1,6 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
 import fs from "fs";
 
 import readline from "readline";
@@ -29,4 +32,41 @@ const fileCreation = () => {
     })
 }
 
+=======
+>>>>>>> 791adb3 (update)
+import fs from "fs";
+
+import readline from "readline";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const fileCreation = () => {
+    rl.question("Enter file Name: ", (name) => {
+        if (fs.existsSync(`${name}.txt`)) {
+            console.log('File with name already exit:');
+            console.log('Retry with new name');            
+            fileCreation();
+        }
+        else {
+            rl.question("Enter the content: ", (content) => {
+                fs.writeFile(`${name}.txt`, content, "utf-8", (err) => {
+                    if (err) {
+                        console.error('Error writing the file');
+                    }
+                    else {
+                        console.log(`File"${name}.txt" created successfully!`);
+                    }
+                })
+            })
+        }
+    })
+}
+
+<<<<<<< HEAD
+=======
+>>>>>>> 2487136 (update)
+>>>>>>> 791adb3 (update)
 fileCreation();
