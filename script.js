0d4d296188ffa485a617a0bdc8809dda11c90f5e
mohit <mohitkumawat221004@gmail.com>
2025-07-26 23:20:45 +0530

    update

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 
-document.getElementById("get-file").textContent = "Generate File";
+const getFile = document.getElementById("get-file")
+const enterFileName = document.getElementById("Enter_file_name");
+const enterFileType = document.getElementById("Enter_file_type");
+const fileLink = document.querySelector(".fileLink");
 
-document.getElementById("get-file").addEventListener("click", function () {
-    const fileName = document.getElementById("Enter_file_name").value.trim();
-    const fileType = document.getElementById("Enter_file_type").value.trim();
 
-    if (!fileName || !fileType) {
+getFile.textContent = "Generate File";
+
+
+getFile.addEventListener("click", function () {
+    const fileName = enterFileName.value.trim();
+    const fileType = enterFileType.value.trim();
+
+    if (!fileName && !fileType) {
         alert("Please enter both file name and file type.");
         return;
     }
+    else if(!fileName) {
+        alert("Please enter file name.");
+        return;
+    }
+    else if(!fileType){
+        alert("Please enter file type.");
+        return;
+    }
+
     const fullName = `${fileName}.${fileType}`;
     const blob = new Blob([""], { type: "text/plain" }); // Empty file
     const url = URL.createObjectURL(blob);
 
-    const linkContainer = document.querySelector(".fileLink");
+    const linkContainer = fileLink;
     linkContainer.innerHTML = `<a href="${url}" download="${fullName}">Download ${fullName}</a>`;
 });
